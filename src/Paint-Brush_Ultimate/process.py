4cf2da502e0280697216edd58e26df6bd1773698
Pedro Henrique Dos Santos <pehenrisans@gmail.com>
2026-06-25 03:13:35 -0300

    Esqueleto de OO adicionado

--- src/Paint-Brush_Ultimate/process.py
+++ src/Paint-Brush_Ultimate/process.py
@@ -10,10 +10,6 @@ class Interface:
     def __init__(self):
         self.janela = tk.Tk()
 
-        self.toolBar = ToolBar(self.janela)
-        self.header = Header(self.janela)
-        self.canvas = TeladPintura(self.janela)
-
         self.tela_configs()
         self.frames()
 
@@ -22,12 +18,11 @@ class Interface:
     def tela_configs(self):
         self.janela.title("Bem Vindo ao Paint Brush Ultimate | clique e arraste para desenhar")
         self.janela.geometry("1280x720")
-        self.janela.title("Janela main")
         self.janela.resizable(False,False)
     
     def frames(self):
-        self.frame_toolBar = Frame(self.janela)
-        self.frame.place(relx = 0.1, rely = 1, relwidth = 0.3, relheight = 1)
+        self.frame_toolBar = tk.Frame(self.janela)
+        self.frame_toolBar.place(relx = 0.1, rely = 1, relwidth = 0.3, relheight = 1)
 
 Interface()
 
