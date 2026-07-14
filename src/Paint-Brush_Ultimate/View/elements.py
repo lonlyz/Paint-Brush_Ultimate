67b65ef0cc5687bc67dda787f76b6873f6f56302
lonlyz <riandandee.10@gmail.com>
2026-07-14 15:23:44 -0300

    ajuste nos botões de salvar e carregar arquivo

--- src/Paint-Brush_Ultimate/View/elements.py
+++ src/Paint-Brush_Ultimate/View/elements.py
@@ -16,8 +16,7 @@ class Toolbar:
         self.ref.Button(self.frame, text="Linha", command=lambda: stateObj.switchState("linha")).pack(side="top", fill="x", pady=5, padx=5)
         self.ref.Button(self.frame, text="Polígono", command=lambda: stateObj.switchState("poligono")).pack(side="top", fill="x", pady=5, padx=5)
         self.ref.Button(self.frame, text="Arco", command=lambda: stateObj.switchState("arco")).pack(side="top", fill="x", pady=5, padx=5)
-        self.ref.Button(self.frame, text="Salvar arquivo", command=lambda: figs.save()).pack(side="top", fill="x", pady=5, padx=5)
-        self.ref.Button(self.frame, text="Abrir arquivo", command=lambda: figs.open()).pack(side="top", fill="x", pady=5, padx=5)
+        
 
         self.btn_cor = self.ref.Button(
             self.frame, 
@@ -29,7 +28,7 @@ class Toolbar:
         self.btn_cor.pack(side="bottom", fill="x", pady=15, padx=5)
 
     def escolher_cor(self):
-       
+        
         cor = colorchooser.askcolor(title="Escolha uma cor para desenhar")
         
         if cor[1]: 
@@ -62,7 +61,7 @@ class Canvas:
         self.stateDict[self.stateObj.currentState].draw(event, cor)
 
     def MarcaInicio(self, event):
-         self.stateDict[self.stateObj.currentState].marca_inicio(event)
+        self.stateDict[self.stateObj.currentState].marca_inicio(event)
 
 class header:
 
@@ -74,4 +73,5 @@ class header:
 
         self.frame = mod.Frame(self.janela,bg = "#31487A",height=60)
         self.frame.pack(side="top", fill="x", padx=10, pady=10)
-        
+        self.ref.Button(self.frame, text="Salvar arquivo", command=lambda: figs.save_json()).pack(side="left", fill="x", pady=5, padx=5)
+        self.ref.Button(self.frame, text="Abrir arquivo", command=lambda: figs.open_json()).pack(side="left", fill="x", pady=5, padx=5)
