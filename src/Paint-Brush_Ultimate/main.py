bd099a46bdc3d7fbb44067a5bc31a8f58a4056dd
thedevPedro <pehenrisans@gmail.com>
2026-07-15 12:47:43 -0300

    Integrate JSON management and update header setup
    Added JSON management functionality and updated header initialization.

--- src/Paint-Brush_Ultimate/main.py
+++ src/Paint-Brush_Ultimate/main.py
@@ -1,18 +1,25 @@
 import tkinter as tk
 from View import elements as Elm
 import Controller.StatesButton as Stb
+from Model import JsonManager as Js
 
 class Interface:
     def __init__(self):
+
+        #Interface Tk e título de cabeçalho
         self.janela = tk.Tk()
         self.janela.title("Paint Brush - Ultimate")
         
+        #Máquina de estados
         self.statemachine = Stb.StateMachine("maoLivre")
 
+        #Barra de ferramentas e do canvas
         self.toolbar = Elm.Toolbar(self.janela, tk, self.statemachine) 
-        self.header = Elm.header(self.janela, tk, self.statemachine) 
         self.canvas = Elm.Canvas(self.janela, tk, self.statemachine, self.toolbar)
         
+        #Gerenciamento do json e do header
+        self.jsonProcess = Js.SaveAndLoad(self.canvas)
+        self.header = Elm.header(self.janela, tk, self.statemachine, self.jsonProcess)
         
         self.janela.mainloop()
 
