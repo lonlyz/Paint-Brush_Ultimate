f3207410150e111c2d0e354165db9c735dad08b7
Pedro Henrique Dos Santos <pehenrisans@gmail.com>
2026-07-15 21:53:40 -0300

    Implementação, salvar e carregar arquivos

--- src/Paint-Brush_Ultimate/Model/FigsDef.py
+++ src/Paint-Brush_Ultimate/Model/FigsDef.py
@@ -4,60 +4,107 @@ class Retangulo(Figuras):
     def __init__(self, canvas):
         self.tela = canvas
         self.ini_x = self.ini_y = self.fim_x = self.fim_y = 0
-        
+        self.drawsFinish = 0 
+        self.arrayDraws = [] 
+
     def marca_inicio(self, event):
         self.ini_x = event.x
         self.ini_y = event.y
+        self.drawsFinish += 1
 
     def draw(self, event, cor):
         self.fim_x = event.x
         self.fim_y = event.y
-        self.tela.delete("preview")
-        self.tela.create_rectangle(self.ini_x, self.ini_y, self.fim_x, self.fim_y, tags="preview", fill=cor)
+        self.tela.delete(str(self.drawsFinish) + "rectangle")
+        self.tela.create_rectangle(
+            self.ini_x, self.ini_y, 
+            self.fim_x, self.fim_y, 
+            tags=str(self.drawsFinish) + "rectangle", 
+            fill=cor
+        )
+
+    def saveToArray(self, event, cor): 
+        self.fim_x = event.x
+        self.fim_y = event.y
+        self.arrayDraws.append([self.ini_x, self.ini_y, self.fim_x, self.fim_y, cor])
+        print("Retângulos salvos!:", self.arrayDraws)
 
 class Oval(Figuras):
     def __init__(self, canvas):
         self.tela = canvas
         self.ini_x = self.ini_y = self.fim_x = self.fim_y = 0
-        
+        self.drawsFinish = 0 
+        self.arrayDraws = [] 
+
     def marca_inicio(self, event):
         self.ini_x = event.x
         self.ini_y = event.y
+        self.drawsFinish += 1
 
     def draw(self, event, cor):
         self.fim_x = event.x
         self.fim_y = event.y
-        self.tela.delete("preview")
-        self.tela.create_oval(self.ini_x, self.ini_y, self.fim_x, self.fim_y, tags="preview", fill=cor)
+        self.tela.delete(str(self.drawsFinish) + "oval")
+        self.tela.create_oval(
+            self.ini_x, self.ini_y, 
+            self.fim_x, self.fim_y, 
+            tags=str(self.drawsFinish) + "oval", 
+            fill=cor
+        )
+
+    def saveToArray(self, event, cor):
+        self.fim_x = event.x
+        self.fim_y = event.y
+        self.arrayDraws.append([self.ini_x, self.ini_y, self.fim_x, self.fim_y, cor])
+        print("Ovais salvas!:", self.arrayDraws)
+
 
 class Linha(Figuras):
     def __init__(self, canvas):
         self.tela = canvas
         self.ini_x = self.ini_y = self.fim_x = self.fim_y = 0
+        self.drawsFinish = 0
+        self.arrayDraws = [] 
         
     def marca_inicio(self, event):
         self.ini_x = event.x
         self.ini_y = event.y
+        self.drawsFinish += 1
 
     def draw(self, event, cor):
         self.fim_x = event.x
         self.fim_y = event.y
-        self.tela.delete("preview") 
-        self.tela.create_line(self.ini_x, self.ini_y, self.fim_x, self.fim_y, tags="preview", fill=cor)
+        self.tela.delete(str(self.drawsFinish) + "linha") 
+        self.tela.create_line(
+            self.ini_x, self.ini_y, 
+            self.fim_x, self.fim_y, 
+            tags=str(self.drawsFinish) + "linha", 
+            fill=cor
+        )
+
+    def saveToArray(self, event, cor):
+        self.fim_x = event.x
+        self.fim_y = event.y
+        self.arrayDraws.append([self.ini_x, self.ini_y, self.fim_x, self.fim_y, cor])
+        print("Linhas salvas!:", self.arrayDraws)
+
 
 class Poligono(Figuras):
     def __init__(self, canvas):
         self.tela = canvas
         self.ini_x = self.ini_y = self.fim_x = self.fim_y = 0
+        self.drawsFinish = 0
+        self.arrayDraws = []
         
     def marca_inicio(self, event):
         self.ini_x = event.x
         self.ini_y = event.y
+        self.drawsFinish += 1
 
     def draw(self, event, cor):
         self.fim_x = event.x
         self.fim_y = event.y
-        self.tela.delete("preview") 
+        self.tela.delete(str(self.drawsFinish) + "polygon") 
         
         ponto3_x = self.ini_x - (self.fim_x - self.ini_x)
         
@@ -65,38 +112,80 @@ class Poligono(Figuras):
             self.ini_x, self.ini_y, 
             self.fim_x, self.fim_y, 
             ponto3_x, self.fim_y, 
-            tags="preview", fill=cor, outline="black"
+            tags=str(self.drawsFinish) + "polygon", 
+            fill=cor, 
+            outline="black"
         )
+
+    def saveToArray(self, event, cor):
+        self.fim_x = event.x
+        self.fim_y = event.y
+        ponto3_x = self.ini_x - (self.fim_x - self.ini_x)
+        
+        
+        triangulo_final = [
+            self.ini_x, self.ini_y, 
+            self.fim_x, self.fim_y, 
+            ponto3_x, self.fim_y, cor
+        ]
+        self.arrayDraws.append(triangulo_final)
+        print("Polígonos salvos!:", self.arrayDraws)
 
 
 class Arco(Figuras):
     def __init__(self, canvas):
         self.tela = canvas
         self.ini_x = self.ini_y = self.fim_x = self.fim_y = 0
+        self.drawsFinish = 0
+        self.arrayDraws = []
 
     def marca_inicio(self, event):
         self.ini_x = event.x
         self.ini_y = event.y
+        self.drawsFinish += 1
 
     def draw(self, event, cor):
         self.fim_x = event.x
         self.fim_y = event.y
-        self.tela.delete("preview")
-        self.tela.create_arc(self.ini_x, self.ini_y, self.fim_x, self.fim_y, fill=cor,tags="preview", style="arc")
+        self.tela.delete(str(self.drawsFinish) + "arc")
+        self.tela.create_arc(
+            self.ini_x, self.ini_y, 
+            self.fim_x, self.fim_y, 
+            fill=cor, 
+            tags=str(self.drawsFinish) + "arc", 
+            style="arc"
+        )
+
+    def saveToArray(self, event, cor):
+        self.fim_x = event.x
+        self.fim_y = event.y
+        self.arrayDraws.append([self.ini_x, self.ini_y, self.fim_x, self.fim_y, cor])
+        print("Arcos salvos!:", self.arrayDraws)
+
 
 class MaoLivre(Figuras):
     def __init__(self, canvas):
         self.tela = canvas
         self.ini_x = self.ini_y = 0
-        
+        self.caminho_atual = [] 
+        self.arrayDraws = [] 
 
     def marca_inicio(self, event):
         self.ini_x = event.x
         self.ini_y = event.y
+        self.caminho_atual = [(self.ini_x, self.ini_y)]
 
     def draw(self, event, cor):
-    
-        self.tela.create_line(self.ini_x, self.ini_y, event.x, event.y, capstyle="round", smooth=True, fill=cor,width=2)
-        
+        self.tela.create_line(
+            self.ini_x, self.ini_y, 
+            event.x, event.y, 
+            capstyle="round", smooth=True, fill=cor, width=2
+        )
         self.ini_x = event.x
         self.ini_y = event.y
+        self.caminho_atual.append((self.ini_x, self.ini_y))
+
+    def saveToArray(self, event, cor):
+        self.arrayDraws.append(list(self.caminho_atual))
+        self.caminho_atual.clear()
+        print("Mão livre salva! Todos os traços:", self.arrayDraws)
--- src/Paint-Brush_Ultimate/View/elements.py
+++ src/Paint-Brush_Ultimate/View/elements.py
@@ -1,5 +1,12 @@
 import Model.FigsDef as figs
 from tkinter import colorchooser
+from Model import JsonManager as jm
+
+
+    #window: recebe a instancia da janela 'Tk()'
+    #mod: recebe a referencia 'tk'
+    #stateObj: recebe a instancia do objeto da máquina de estados
+    #jsonObj: recebe a instancia do objeto gestor de salvamento e carregamento de dados
 
 class Toolbar:
     def __init__(self, window, mod, stateObj):
@@ -52,8 +59,10 @@ class Canvas:
             "arco": figs.Arco(self.canvas)
         }      
 
-        self.canvas.bind('<B1-Motion>', self.Create)
         self.canvas.bind('<Button-1>', self.MarcaInicio)
+        self.canvas.bind('<B1-Motion>', self.Create)
+        self.canvas.bind('<ButtonRelease-1>', self.FinishDraw)
+    
 
     def Create(self, event):
         cor = self.toolbarObj.cor_atual
@@ -61,15 +70,21 @@ class Canvas:
 
     def MarcaInicio(self, event):
          self.stateDict[self.stateObj.currentState].marca_inicio(event)
+    
+    def FinishDraw(self, event):
+         cor = self.toolbarObj.cor_atual
+         self.stateDict[self.stateObj.currentState].saveToArray(event,cor)
 
 class header:
 
-    def __init__(self,window,mod,stateObj):
+    def __init__(self, window, mod, stateObj, JmObj):
 
         self.janela = window
         self.ref = mod
         self.stateObj = stateObj
+        
 
         self.frame = mod.Frame(self.janela,bg = "#31487A",height=60)
         self.frame.pack(side="top", fill="x", padx=10, pady=10)
-        
+        self.ref.Button(self.frame, text="Salvar arquivo", command=lambda: JmObj.save()).pack(side="left", fill="x", pady=5, padx=5)
+        self.ref.Button(self.frame, text="Abrir arquivo", command=lambda: JmObj.load()).pack(side="left", fill="x", pady=5, padx=5)
